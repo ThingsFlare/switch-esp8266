7bc851c4be85e43dac6d3e53bd3276fbed71604f
Srinath Janakiraman <me@vjsrinath.com>
2020-07-26 12:55:46 +0530

    Switch using webthings and single channel relay

--- src/main.cpp
+++ src/main.cpp
@@ -1,110 +1,35 @@
-#include <FS.h>
-#include <ThingsBoard.h>
-
-#include <ESP8266WiFi.h>
+#define ARDUINOJSON_USE_LONG_LONG 1
+#include <Arduino.h>
+#include "Thing.h"
+#include "WebThingAdapter.h"
+// #include <ESP8266WiFi.h>
 #include <ArduinoJson.h>
-
+#include <ESP_DoubleResetDetector.h>
 #include <ESPAsyncWebServer.h>
-#include <ESPAsyncWiFiManager.h> //https://github.com/tzapu/WiFiManager
-                                 // #include "SoftwareSerial.h"
+#include <ESPAsyncWiFiManager.h>
+#include <LittleFS.h>
 
-// See https://thingsboard.io/docs/getting-started-guides/helloworld/
-// to understand how to obtain an access token
+// Number of seconds after reset during which a
+// subseqent reset will be considered a double reset.
+#define DRD_TIMEOUT 10
+// RTC Memory Address for the DoubleResetDetector to use
+#define DRD_ADDRESS 0
 
-// // Baud rate for serial debug
-// #define SERIAL_DEBUG_BAUD 9600
-// // Baud rate for communicating with ESP chip
-// #define SERIAL_ESP8266_BAUD 9600
+const int outputPin = 5;
+char thing_id[50];
+char thing_name[50];
 
-// Serial driver for ESP
-// SoftwareSerial soft(2, 3); // RX, TX
-// Initialize the Ethernet client object
-// WiFiEspClient espClient;
-// Initialize ThingsBoard instance
-#define VALUE_KEY "value"
-#define COUNT_OF(x) ((sizeof(x) / sizeof(0 [x])) / ((size_t)(!(sizeof(x) % sizeof(0 [x])))))
-#define SWITCH_PIN 5
-#define DEVICE_ID "Motor-Relay-01"
-
-class CustomLogger
-{
-public:
-  static void log(const char *msg)
-  {
-    Serial.print("[Serial Logger] ");
-    Serial.println(msg);
-  }
-};
-
-WiFiClient wifiClient;
-ThingsBoardSized<128, 32, CustomLogger> tb(wifiClient);
-
+DoubleResetDetector *drd;
 AsyncWebServer server(80);
 DNSServer dns;
-bool subscribed = false;
-bool lastValue = false;
+WebThingAdapter *adapter;
+const char *apName;
 
-void updateValueTelemetry(bool value)
-{
-  Serial.println("Sending telemetry data...");
-  const int data_items = 1;
-  Telemetry data[data_items] = {
-      {"value", value}};
+const char *relayTypes[] = {"OnOffSwitch", "Relay", nullptr};
+ThingDevice relay("relay", "relay", relayTypes);
+ThingProperty relayStatus("on", "", BOOLEAN, "OnOffProperty");
 
-  // // Uploads new telemetry to ThingsBoard using MQTT.
-  // // See https://thingsboard.io/docs/reference/mqtt-api/#telemetry-upload-api
-  // // for more details
-  tb.sendTelemetry(data, data_items);
-}
-
-RPC_Response setValue(const RPC_Data &data)
-{
-  bool enabled = data[VALUE_KEY];
-  lastValue = enabled;
-  Serial.println("Received rpc request,");
-  Serial.print("value:");
-  Serial.print(enabled);
-  updateValueTelemetry(enabled);
-  digitalWrite(SWITCH_PIN, enabled);
-  return RPC_Response(VALUE_KEY, lastValue);
-}
-
-RPC_Response getValue(const RPC_Data &data)
-{
-  return RPC_Response(VALUE_KEY, lastValue);
-}
-
-RPC_Callback callbacks[] = {
-    {"setValue", setValue},
-    {"getValue", getValue},
-};
-
-void subscribeToRPC()
-{
-  // Subscribe for RPC, if needed
-  if (!subscribed)
-  {
-    Serial.println("Subscribing for RPC...");
-
-    // Perform a subscription. All consequent data processing will happen in
-    // callbacks as denoted by callbacks[] array.
-    if (!tb.RPC_Subscribe(callbacks, COUNT_OF(callbacks)))
-    {
-      Serial.println("Failed to subscribe for RPC");
-      return;
-    }
-
-    Serial.println("Subscribed with ");
-    Serial.print(COUNT_OF(callbacks));
-    Serial.print(" callbacks");
-
-    Serial.println("Subscribe done");
-    subscribed = true;
-  }
-}
-
-char tb_server[100];
-char tb_token[34] = "YOUR_DEVICE_TOKEN";
+bool lastOn = false;
 
 //flag for saving data
 bool shouldSaveConfig = false;
@@ -116,11 +41,17 @@ void saveConfigCallback()
   shouldSaveConfig = true;
 }
 
+//callback notifying us of the need to save config
+void onConfigMode(AsyncWiFiManager *manager)
+{
+  drd->stop();
+}
+
 void saveConfig(DynamicJsonDocument &json)
 {
   Serial.println("saving config");
 
-  File configFile = SPIFFS.open("/config.json", "w");
+  File configFile = LittleFS.open("/config.json", "w");
   if (!configFile)
   {
     Serial.println("failed to open config file for writing");
@@ -132,28 +63,18 @@ void saveConfig(DynamicJsonDocument &json)
   configFile.close();
 }
 
-void setup()
+void configure(bool resetConfig)
 {
+  strcpy(thing_id, apName);
 
-  // put your setup code here, to run once:
-  Serial.begin(115200);
-  Serial.println();
-  pinMode(SWITCH_PIN, OUTPUT);
-
-  //clean FS, for testing
-  //SPIFFS.format();
-
-  //read configuration from FS json
-  Serial.println("mounting FS...");
-
-  if (SPIFFS.begin())
+  if (LittleFS.begin())
   {
     Serial.println("mounted file system");
-    if (SPIFFS.exists("/config.json"))
+    if (LittleFS.exists("/config.json"))
     {
       //file exists, reading and loading
       Serial.println("reading config file");
-      File configFile = SPIFFS.open("/config.json", "r");
+      File configFile = LittleFS.open("/config.json", "r");
       if (configFile)
       {
         Serial.println("opened config file");
@@ -162,7 +83,7 @@ void setup()
         std::unique_ptr<char[]> buf(new char[size]);
 
         configFile.readBytes(buf.get(), size);
-        DynamicJsonDocument json(1024);
+        DynamicJsonDocument json(100);
         DeserializationError error = deserializeJson(json, buf.get());
 
         serializeJson(json, Serial);
@@ -173,9 +94,8 @@ void setup()
         else
         {
           Serial.println("\nparsed json");
-
-          strcpy(tb_server, json["tb_server"]);
-          strcpy(tb_token, json["tb_token"]);
+          strcpy(thing_id, json["thing_id"]);
+          strcpy(thing_name, json["thing_name"]);
         }
       }
     }
@@ -184,45 +104,29 @@ void setup()
   {
     Serial.println("failed to mount FS");
   }
-  //end read
 
-  // The extra parameters to be configured (can be either global or just in the setup)
-  // After connecting, parameter.getValue() will get you the configured value
-  // id/name placeholder/prompt default length
-  AsyncWiFiManagerParameter custom_tb_server("server", "Thingsboard server", tb_server, 100);
-  AsyncWiFiManagerParameter custom_tb_token("token", "Access Token", tb_token, 32);
+  // Create custom parameters to be displayed in wifi configuration page
+  AsyncWiFiManagerParameter thing_id_param("thing_id", "Thing Id", thing_id, 50);
+  AsyncWiFiManagerParameter thing_name_param("thing_name", "Thing Display Name", thing_name, 50);
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
   AsyncWiFiManager wifiManager(&server, &dns);
 
+  wifiManager.setAPCallback(onConfigMode);
   //set config save notify callback
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
-  //set static ip
-  // wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+  wifiManager.addParameter(&thing_id_param);
+  wifiManager.addParameter(&thing_name_param);
+  if (resetConfig)
+  {
+    wifiManager.resetSettings();
+    drd->stop();
+    ESP.reset();
+  }
 
-  //add all your parameters here
-  wifiManager.addParameter(&custom_tb_server);
-  wifiManager.addParameter(&custom_tb_token);
-
-  //reset settings - for testing
-  // wifiManager.resetSettings();
-
-  //set minimu quality of signal so it ignores AP's under that quality
-  //defaults to 8%
-  //wifiManager.setMinimumSignalQuality();
-
-  //sets timeout until configuration portal gets turned off
-  //useful to make it all retry or go to sleep
-  //in seconds
-  //wifiManager.setTimeout(120);
-
-  //fetches ssid and pass and tries to connect
-  //if it does not connect it starts an access point with the specified name
-  //here  "AutoConnectAP"
-  //and goes into a blocking loop awaiting configuration
-  if (!wifiManager.autoConnect(DEVICE_ID))
+  if (!wifiManager.autoConnect(apName))
   {
     Serial.println("failed to connect and hit timeout");
     delay(3000);
@@ -234,18 +138,22 @@ void setup()
   //if you get here you have connected to the WiFi
   Serial.println("connected...yeey :)");
 
-  //read updated parameters
-  strcpy(tb_server, custom_tb_server.getValue());
-  strcpy(tb_token, custom_tb_token.getValue());
+  strcpy(thing_id, thing_id_param.getValue());
+  strcpy(thing_name, thing_name_param.getValue());
+
+  if (thing_name_param.getValueLength() < 1)
+  {
+    strcpy(thing_name, thing_id_param.getValue());
+  }
 
   //save the custom parameters to FS
   if (shouldSaveConfig)
   {
     Serial.println("saving config");
 
-    DynamicJsonDocument json(1024);
-    json["tb_server"] = tb_server;
-    json["tb_token"] = tb_token;
+    DynamicJsonDocument json(150);
+    json["thing_id"] = thing_id;
+    json["thing_name"] = thing_name;
 
     saveConfig(json);
   }
@@ -254,41 +162,42 @@ void setup()
   Serial.println(WiFi.localIP());
 }
 
-void loop()
+void setup(void)
 {
-  delay(1000);
+  String adapterName = "Relay-" + String(ESP.getChipId());
+  apName = adapterName.c_str();
 
-  if (!tb.connected())
+  pinMode(outputPin, OUTPUT);
+  digitalWrite(outputPin, LOW);
+  Serial.begin(115200);
+
+  drd = new DoubleResetDetector(DRD_TIMEOUT, DRD_ADDRESS);
+  bool resetRequested = drd->detectDoubleReset();
+  configure(resetRequested);
+
+  adapter = new WebThingAdapter(adapterName, WiFi.localIP());
+  relay.id = thing_id;
+  relay.title = thing_name;
+  relay.addProperty(&relayStatus);
+  adapter->addDevice(&relay);
+  adapter->begin();
+  Serial.println("HTTP server started");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.print("/things/");
+  Serial.println(relay.id);
+}
+
+void loop(void)
+{
+  adapter->update();
+  bool on = relayStatus.getValue().boolean;
+  digitalWrite(outputPin, on ? HIGH : LOW); // active low relay
+  if (on != lastOn)
   {
-    // Connect to the ThingsBoard
-    Serial.print("Connecting to: ");
-    Serial.print(tb_server);
-    Serial.print(" with token ");
-    Serial.println(tb_token);
-    if (tb.connect(tb_server, tb_token))
-    {
-      // setting the motor swith to off when the device restarts
-      updateValueTelemetry(false);
-    }
-
-    Serial.println("Failed to connect, retrying ...");
-    return;
+    Serial.print(relay.id);
+    Serial.print(": ");
+    Serial.println(on);
   }
-
-  subscribeToRPC();
-
-  Serial.println("Sending attributes data...");
-
-  const int attribute_items = 3;
-  Attribute attributes[attribute_items] = {
-      {"device_type", "switch"},
-      {"active", true},
-      {"isOn", lastValue},
-  };
-
-  // Publish attribute update to ThingsBoard using MQTT.
-  // See https://thingsboard.io/docs/reference/mqtt-api/#publish-attribute-update-to-the-server
-  // for more details
-  tb.sendAttributes(attributes, attribute_items);
-  tb.loop();
+  lastOn = on;
 }
